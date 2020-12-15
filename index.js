1968dcee0514ddd110f0f79cf72a7c84090e1be9
Isaac Mackle <isaac.mackle@dominos.com.au>
2020-12-15 18:02:28 +1300

    chore: add login with auth

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+
 const authRoutes = require("./routes/auth");
+const dashboardRoutes = require("./routes/dashboard");
+const verifyToken = require("./routes/validate-token");
 
 // middleware
 app.use(express.json()); // body parser
+
 app.use("/api/user", authRoutes);
+app.use("/api/dashboard", verifyToken, dashboardRoutes);
 
 app.listen(3000, () => console.log("🚀 server is running...."));
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,11 @@ const doesEmailExist = require("../dynamodb/queryEmail");
 const addUser = require("../model/user");
 const router = require("express").Router();
 const { registerValidation } = require("../validation");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 router.post("/register", async (req, res) => {
     // validate the user
@@ -14,19 +19,60 @@ router.post("/register", async (req, res) => {
     if (isEmailExist.Count > 0)
         return res.status(400).json({ error: "Email already exists" });
 
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+
     const user = {
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password
     };
 
     try {
-        const savedUser = await addUser(user);
-        res.json({ error: null, data: "User Created" });
+        await addUser(user);
+        res.json({ error: null, data: { message: "User Created" } });
     } catch (error) {
         console.log(error);
         res.status(400).json({ error });
     }
+});
+
+router.post("/login", async (req, res) => {
+    // validate the user
+    const { error } = registerValidation(req.body);
+
+    // throw validation errors
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    const user = await doesEmailExist(req.body.email);
+    if (user.Count === 0)
+        return res.status(400).json({ error: "Email is wrong" });
+
+    // check for password correctness
+    const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.Items[0].info.password
+    );
+
+    if (!isValidPassword)
+        return res.status(400).json({ error: "Password is wrong" });
+
+    // Create token
+    const token = jwt.sign(
+        {
+            name: user.Items[0].info.name
+        },
+        process.env.TOKEN_SECRET
+    );
+
+    res.header("auth-token", token).json({
+        error: null,
+        data: {
+            message: "Login successful",
+            token
+        }
+    });
 });
 
 module.exports = router;
